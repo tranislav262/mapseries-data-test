7e3f2e57e7504be4ea3cab9129a1a09ef6afcc70
jirduf <jirduf@users.noreply.github.com>
2015-12-04 13:53:01 +0100

    Updated config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 var mapseries = {};
 mapseries.config = {
   formatFunctionsTemplate: {
-    addDegrees: function(stringValue, sheet) {
+    addDegrees: function (stringValue, sheet) {
       stringValue += '';
       if(stringValue.length==4) {
         stringValue = stringValue.substr(0,2)+'°'+stringValue.substr(2)+'°';
@@ -10,5 +10,19 @@ mapseries.config = {
     }
   },
   series: [
+    {
+      title: "Středovýchodní Evropa: Třetí vojenské mapování, speciální mapy 1:75 000",
+      layer: "stredovychodni-evropa-treti-vojenske-mapovani-specialni-mapy-175-000",
+      template: "stredovychodni-evropa-treti-vojenske-mapovani-specialni-mapy-175-000.txt",
+      formatFunctions: {
+        addDegrees: function (stringValue, sheet) {
+          stringValue += '';
+          if(stringValue.length==4) {
+            stringValue = stringValue.substr(0,2)+'°'+stringValue.substr(2)+'°';
+          }
+          return stringValue;
+        }
+      }
+    }
   ]
-};
+}

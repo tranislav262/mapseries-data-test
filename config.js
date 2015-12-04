6daccaca634235313afcb39180a55a4a0d4b1316
Erich Duda <dudaerich@gmail.com>
2015-12-04 13:26:50 +0100

    Clear data

--- config.js
+++ config.js
@@ -10,47 +10,5 @@ mapseries.config = {
     }
   },
   series: [
-    {
-      title: "Austria-Hungary: 3rd Military Survey, 1:75 K (RDA 2015)",
-      layer: "austria-hungary--3rd-military-survey--75",
-      template: "ah-ms3-75_rda2015.txt",
-      formatFunctions: {
-        addDegrees: function(stringValue, sheet) {
-          stringValue += '';
-          if(stringValue.length==4) {
-            stringValue = stringValue.substr(0,2)+'°'+stringValue.substr(2)+'°';
-          }
-          return stringValue;
-        }
-      }
-    },
-    {
-      title: "Austria-Hungary: 3rd Military Survey, 1:200 K (RDA 2015)",
-      layer: "austria-hungary--3rd-military-survey--200",
-      template: "ah-ms3-200_rda2015.txt",
-      formatFunctions: {
-        addDegrees: function(stringValue, sheet) {
-          stringValue += '';
-          if(stringValue.length==4) {
-            stringValue = stringValue.substr(0,2)+'°'+stringValue.substr(2)+'°';
-          }
-          return stringValue;
-        }
-      }
-    },
-    {
-      title: "Austria-Hungary: 3rd Military Survey, 1:200 K",
-      layer: "austria-hungary--3rd-military-survey--200",
-      template: "ah-ms3-200.txt",
-      formatFunctions: {
-        addDegrees: function(stringValue, sheet) {
-          stringValue += '';
-          if(stringValue.length==4) {
-            stringValue = stringValue.substr(0,2)+'°'+stringValue.substr(2)+'°';
-          }
-          return stringValue;
-        }
-      }
-    }
   ]
-}
+};

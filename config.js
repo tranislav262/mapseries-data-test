40bb5f6fd1099d9d9aef163ce6a4e6fa5742fe52
mila.pacek <mila.pacek@gmail.com>
2020-04-16 09:36:28 +0200

    Updated Evropa: Třetí vojenské mapování, speciální mapy 1:75 000 serie

--- config.js
+++ config.js
@@ -68,7 +68,7 @@ mapseries.config = {
     },
     {
       title: "Evropa: Geologická speciální mapa 1:75 000",
-      layer: "evropa-geologicka-specialni-mapa-175-000",
+      layer: "evropa-treti-vojenske-mapovani-specialni-mapy-175-000",
       template: "evropa-geologicka-specialni-mapa-175-000.txt",
       formatFunctions: {
         addDegrees: function (stringValue, sheet) {
